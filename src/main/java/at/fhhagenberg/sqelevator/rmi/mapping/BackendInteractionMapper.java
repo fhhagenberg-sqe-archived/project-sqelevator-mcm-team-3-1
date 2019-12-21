cf0bf1ba3e6c0feb1594d2013c9cbf8be18e8b01
jmy <joshy198@gmx.at>
2019-12-21 23:50:01 +0100

    added first bunch of interfaces and classes

---
+++ src/main/java/at/fhhagenberg/sqelevator/rmi/mapping/BackendInteractionMapper.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.fhhagenberg.sqelevator.rmi.mapping;
+
+import at.fhhagenberg.sqelevator.interfaces.IBackendInteractionMapper;
+
+/**
+ *
+ * @author jmayr
+ */
+public class BackendInteractionMapper implements IBackendInteractionMapper{
+    
+}
---
+++ src/main/java/at/fhhagenberg/sqelevator/ui/ElevatorFxGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.fhhagenberg.sqelevator.ui;
+
+/**
+ *
+ * @author jmayr
+ * 
+ * NOTES from the lecture:
+ * in the used Objects
+ * someclasspropertyProperty(): StringProperty; adding change listeners for javaFX
+ * also provides binding properties
+ * WARNING: JAVA FX PROPERITARY!!!! SO INTERMEDIATE OBJECTS HAVE TO BE CREATED!!!!
+ * 
+ */
+public class ElevatorFxGUI {
+    
+}
