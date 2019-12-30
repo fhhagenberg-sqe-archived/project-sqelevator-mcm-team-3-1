f4cdc1124daf59f57c12c2ff2dc65dcb498c055e
jmy <joshy198@gmx.at>
2019-12-31 00:48:37 +0100

    started implementing FX UI

--- src/main/java/at/fhhagenberg/sqelevator/ui/FXElevator.java
+++ src/main/java/at/fhhagenberg/sqelevator/ui/FXElevator.java
@@ -6,18 +6,70 @@
 package at.fhhagenberg.sqelevator.ui;
 
 import at.fhhagenberg.sqelevator.interfaces.ILocalElevator;
-import javafx.scene.Node;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 /**
  *
  * @author jmayr
  */
-public class FXElevator extends VBox {
-    
-    private ILocalElevator E;
-    public FXElevator(){
-    
+public class FXElevator extends GridPane {
+
+    private ILocalElevator e;
+    private int numberOfFloors;
+    private Pane[] floors;
+    private HBox header;
+
+    public FXElevator(ILocalElevator e, int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+        this.e = e;
+        floors = new Pane[numberOfFloors];
+        this.add(header, 0, 0, 2, 1);
+        for (int i = 0; i < numberOfFloors; i++) {
+            floors[i] = new Pane();
+            this.add(floors[i], i + 1, 0);
+        }
     }
-    
+
+    public void updateView() {
+        clearAll();
+        for (int floor : this.e.getSelectedFloors()) {
+            this.setSelection(floor);
+        }
+        this.setPosition(this.e.getCurrentFloor());
+        this.setTarget(this.e.getTargetFloor());
+    }
+
+    private void clearAll() {
+        for (Pane p : floors) {
+            p.setBackground(Background.EMPTY);
+            p.setBorder(Border.EMPTY);
+        }
+    }
+
+    private void setTarget(int floor) {
+        floors[floor].setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN,
+                CornerRadii.EMPTY, Insets.EMPTY)));
+    }
+
+    private void setPosition(int floor) {
+        floors[floor].setBackground(new Background(new BackgroundFill(Color.DARKGREEN,
+                CornerRadii.EMPTY, Insets.EMPTY)));
+    }
+
+    private void setSelection(int floor) {
+        floors[floor].setBorder(new Border(new BorderStroke(Color.LIGHTBLUE,
+                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+    }
+
 }
